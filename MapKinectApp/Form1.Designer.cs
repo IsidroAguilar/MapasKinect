a79ab732b5c54fba422674fc974c504a2a6abd05
Javier <javierat93@hotmail.com>
2014-11-27 00:11:50 -0600

    Commit

--- MapKinectApp/Form1.Designer.cs
+++ MapKinectApp/Form1.Designer.cs
@@ -29,19 +29,49 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imgMapa = new System.Windows.Forms.PictureBox();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lblShoulderPosition = new System.Windows.Forms.Label();
+            this.lblShoulder = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgMapa)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // imgMapa
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(21, 31);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(623, 470);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imgMapa.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("imgMapa.BackgroundImage")));
+            this.imgMapa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgMapa.Location = new System.Drawing.Point(21, 31);
+            this.imgMapa.Name = "imgMapa";
+            this.imgMapa.Size = new System.Drawing.Size(623, 470);
+            this.imgMapa.TabIndex = 0;
+            this.imgMapa.TabStop = false;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(677, 104);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.TabIndex = 6;
+            this.lblMensaje.Text = "label1";
+            // 
+            // lblShoulderPosition
+            // 
+            this.lblShoulderPosition.AutoSize = true;
+            this.lblShoulderPosition.Location = new System.Drawing.Point(677, 69);
+            this.lblShoulderPosition.Name = "lblShoulderPosition";
+            this.lblShoulderPosition.Size = new System.Drawing.Size(35, 13);
+            this.lblShoulderPosition.TabIndex = 5;
+            this.lblShoulderPosition.Text = "label1";
+            // 
+            // lblShoulder
+            // 
+            this.lblShoulder.AutoSize = true;
+            this.lblShoulder.Location = new System.Drawing.Point(677, 31);
+            this.lblShoulder.Name = "lblShoulder";
+            this.lblShoulder.Size = new System.Drawing.Size(35, 13);
+            this.lblShoulder.TabIndex = 4;
+            this.lblShoulder.Text = "label1";
             // 
             // Form1
             // 
@@ -49,18 +79,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(832, 525);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblShoulderPosition);
+            this.Controls.Add(this.lblShoulder);
+            this.Controls.Add(this.imgMapa);
             this.Name = "Form1";
             this.Text = "Manipulación de Mapas con Kinect";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgMapa)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgMapa;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label lblShoulderPosition;
+        private System.Windows.Forms.Label lblShoulder;
     }
 }
 
--- MapKinectApp/Form1.cs
+++ MapKinectApp/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Kinect;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace MapKinectApp
 {
@@ -19,8 +21,12 @@ namespace MapKinectApp
         }
 
         #region Variable Globales
+
         KinectSensor sensor = new KinectSensor();
-        Skeleton[] skeletons;
+        Skeleton[] Skeletons;
+        int i = 0;
+        int shoulderA = 0;
+
         #endregion
 
         #region Metodos Generales
@@ -33,11 +39,80 @@ namespace MapKinectApp
             sensor.AllFramesReady += FramesReady;
             sensor.Start();
         }
+
+        Bitmap ImageToBitmap(ColorImageFrame Image)
+        {
+            byte[] pixeldata = new byte[Image.PixelDataLength];
+            Image.CopyPixelDataTo(pixeldata);
+            Bitmap bmap = new Bitmap(
+                Image.Width,
+                Image.Height,
+                PixelFormat.Format32bppRgb);
+            BitmapData bmapdata = bmap.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.WriteOnly, bmap.PixelFormat);
+            IntPtr ptr = bmapdata.Scan0;
+            Marshal.Copy(pixeldata, 0, ptr, Image.PixelDataLength);
+            bmap.UnlockBits(bmapdata);
+            return bmap;
+        }
+
+        void FramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            ColorImageFrame VFrame = e.OpenColorImageFrame();
+            if (VFrame == null) return;
+            byte[] pixeldata = new byte[VFrame.PixelDataLength];
+            Bitmap bmap = ImageToBitmap(VFrame);
+            //Graphics g = Graphics.FromImage(bmap);
+            //g.DrawEllipse(Pens.Red, new Rectangle(x - 10, y - 10, 20, 20));
+            imgMapa.Image = bmap;
+
+            SkeletonFrame SFrame = e.OpenSkeletonFrame();
+            if (SFrame == null) return;
+
+            Skeletons = new Skeleton[SFrame.SkeletonArrayLength];
+            SFrame.CopySkeletonDataTo(Skeletons);
+            foreach (Skeleton unSkeleton in Skeletons)
+            {
+                if (unSkeleton.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    lblShoulderPosition.Text = "X:" +
+                        unSkeleton.Joints[JointType.ShoulderRight].Position.X.ToString()
+                        + " Y:" + unSkeleton.Joints[JointType.ShoulderRight].Position.Y.ToString() + " Z:"
+                        + unSkeleton.Joints[JointType.ShoulderRight].Position.Y.ToString();
+                    lblMensaje.Text = "Mueve..." + i++;
+                    SkeletonPoint Sloc = unSkeleton.Joints[JointType.HandRight].Position;
+                    ColorImagePoint Cloc = sensor.MapSkeletonPointToColor(Sloc, ColorImageFormat.RgbResolution640x480Fps30);
+                    markAtPoint(Cloc, bmap);
+                    Sloc = unSkeleton.Joints[JointType.HandLeft].Position;
+                    Cloc = sensor.MapSkeletonPointToColor(Sloc, ColorImageFormat.RgbResolution640x480Fps30);
+                    markAtPoint(Cloc, bmap);
+                    //imgMapa.Image = bmap;
+                }
+
+                if (unSkeleton.TrackingState == SkeletonTrackingState.PositionOnly)
+                {
+                    if (unSkeleton.Joints[JointType.ShoulderRight].TrackingState == JointTrackingState.Tracked)
+                        lblShoulder.Text = "Mueve..." + shoulderA++;
+                }
+            }
+        }
+
+        void markAtPoint(ColorImagePoint p, Bitmap bmp)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            SolidBrush colorPoint = new SolidBrush(Color.Navy);
+            g.FillEllipse(colorPoint, new Rectangle(p.X - 5, p.Y - 5, 10, 10));
+        }
+
         #endregion
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            InicializaSensor();
+        }
 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sensor.Stop();
         }
     }
 }
